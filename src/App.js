01ad4f3b39bafa6b4ba08a5d7064fa5f5149b491
Doriane <berdalado@gmail.com>
2021-04-18 13:34:06 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import DisplayEmployee from "./components/DisplayEmployee";
-import useState from "react";
+import React from "react";
 import axios from "axios";
 
-const sampleEmployee = {
-  gender: "male",
-  name: {
-    first: "Charlie",
-    last: "Thompson",
-  },
-  location: {
-    street: {
-      number: 761,
-      name: "Tay Street",
-    },
-    city: "Timaru",
-    postcode: 76111,
-  },
-  email: "charlie.thompson@example.com",
-  picture: {
-    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
-  },
-};
-
-/*function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <DisplayEmployee employee={sampleEmployee} />
-      </header>
-    </div>
-  );
-} */
-
 function App() {
-  const [employee, setEmployee] = useState(sampleEmployee);
+  const [employee, setEmployee] = React.useState("");
   const getEmployee = () => {
     // Send the request
     axios
@@ -47,7 +17,6 @@ function App() {
         setEmployee(data.results[0]);
       });
   };
-
   return (
     <div className='App'>
       <DisplayEmployee employee={employee} />
